ae768ea3272cfcd54c87f51a7d7aea3c91c7c812
Jasz-rgb <jasminesahoo007@gmail.com>
2025-06-14 02:19:25 +0530

    added WASD to camera

--- main.cpp
+++ main.cpp
@@ -14,6 +14,13 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+//camera
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+float deltaTime = 0.0f;	// Time between current frame and last frame
+float lastFrame = 0.0f; // Time of last frame
+
 std::string loadShaderSource(const std::string& path) {
     std::ifstream shaderFile(path);
     std::stringstream shaderStream;
@@ -30,6 +37,16 @@ void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+    // camera user control 
+    const float cameraSpeed = 2.5f * deltaTime; // adjust accordingly
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
 
 int main()
@@ -268,19 +285,24 @@ int main()
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         /*glClear(GL_COLOR_BUFFER_BIT);*/
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glUseProgram(shaderProgram);
+        glm::mat4 view = glm::mat4(1.0f);
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         // Set the camera/view transformation
-        const float radius = 10.0f;
+        /*const float radius = 10.0f;
         float camX = sin(glfwGetTime()) * radius;
-        float camZ = cos(glfwGetTime()) * radius;
-        glm::mat4 view = glm::lookAt(
-            glm::vec3(camX, 0.0f, camZ),  // camera position
-            glm::vec3(0.0f, 0.0f, 0.0f),  // look at origin
-            glm::vec3(0.0f, 1.0f, 0.0f)   // up vector
-        );
+        float camZ = cos(glfwGetTime()) * radius;*/
+        //glm::mat4 view = glm::lookAt(
+        //    glm::vec3(camX, 0.0f, camZ),  // camera position
+        //    glm::vec3(0.0f, 0.0f, 0.0f),  // look at origin
+        //    glm::vec3(0.0f, 1.0f, 0.0f)   // up vector
+        //);
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
         //// ADD YOUR TRANSFORMATION MATRIX HERE
         //glm::mat4 trans = glm::mat4(1.0f);
@@ -305,13 +327,14 @@ int main()
         glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
         // --- Bind VAO before drawing ---
         glBindVertexArray(VAO);
+        int modelLoc = glGetUniformLocation(shaderProgram, "model");
         for (unsigned int i = 0; i < 10; i++)
         {
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
             float angle = 20.0f * i + glfwGetTime() * 20.0f; // Optional: rotate over time
             model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
