29c33939c486e888564b2de2184fdb7350828045
Jasz-rgb <jasminesahoo007@gmail.com>
2025-06-13 22:57:51 +0530

    rotation over time of the rectangle/square

--- main.cpp
+++ main.cpp
@@ -216,6 +216,21 @@ int main()
         unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
+        // model, view, projection :
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+        glm::mat4 view = glm::mat4(1.0f);
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        glm::mat4 projection;
+        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+        int modelLoc = glGetUniformLocation(shaderProgram, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        int viewLoc = glGetUniformLocation(shaderProgram, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+        
+
         //glUniform1i(glGetUniformLocation(shaderProgram, "texture1"), 0);
         /*float timeValue = glfwGetTime();
         float greenValue = sin(timeValue) / 2.0f + 0.5f;
--- vertex_shader.glsl
+++ vertex_shader.glsl
@@ -7,9 +7,13 @@ out vec3 outColor;
 out vec2 TexCoord;
 uniform mat4 transform; // Uniform for transformation matrix
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
-    gl_Position = transform * vec4(aPos, 1.0); // see how we directly give a vec3 to vec4's constructor
+    gl_Position = projection* view*model * vec4(aPos, 1.0); // see how we directly give a vec3 to vec4's constructor
     outColor = aColor;
     TexCoord = vec2(aTexCoord.x, aTexCoord.y); // Pass texture coordinates to fragment shader
 }
