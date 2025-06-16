c86a610058ac932d75d20661a6056bee9579176d
Jasz-rgb <jasminesahoo007@gmail.com>
2025-06-17 01:55:08 +0530

    light color transition using glfwGetTime function

--- light_main.cpp
+++ light_main.cpp
@@ -162,21 +162,30 @@ int main() {
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera.GetViewMatrix();
 
-        // Draw container
+        // light properties
         lightingShader.use();
+        glm::vec3 lightColor;
+        lightColor.x = static_cast<float>(sin(glfwGetTime() * 2.0));
+        lightColor.y = static_cast<float>(sin(glfwGetTime() * 0.7));
+        lightColor.z = static_cast<float>(sin(glfwGetTime() * 1.3));
+        glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f); // decrease the influence
+        glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f); // low influence
+        lightingShader.setVec3("light.ambient", ambientColor);
+        lightingShader.setVec3("light.diffuse", diffuseColor);
+        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
+        // Draw container
         lightingShader.setMat4("projection", projection);
         lightingShader.setMat4("view", view);
-        lightingShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
-        lightingShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
-        lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
-        lightingShader.setFloat("material.shininess", 32.0f);
+        lightingShader.setVec3("material.ambient", 0.19125f, 0.0735f, 0.0225f);
+        lightingShader.setVec3("material.diffuse", 0.7038f, 0.27048f, 0.0828f);
+        lightingShader.setVec3("material.specular", 0.256777f, 0.137622f, 0.086014f);
+        lightingShader.setFloat("material.shininess", 0.1f * 128.0f); // 12.8
         lightingShader.setVec3("viewPos", camera.Position);
         //lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
         //lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
         lightingShader.setVec3("light.position", lightPos);
-        lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-        lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f); // darken diffuse light a bit
-        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
         glm::mat4 model = glm::mat4(1.0f);
         lightingShader.setMat4("model", model);
         glBindVertexArray(containerVAO);
