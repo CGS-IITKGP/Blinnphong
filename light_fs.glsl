6cc782d25f94e3c3254c3b05f51557979169020f
Jasz-rgb <jasminesahoo007@gmail.com>
2025-06-17 01:33:22 +0530

    material

--- light_fs.glsl
+++ light_fs.glsl
@@ -1,33 +1,48 @@
 #version 330 core
+struct Material {
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+}; 
+struct Light {
+    vec3 position;
+  
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Light light;  
+
+uniform Material material;
 in vec3 FragPos;
 in vec3 Normal;
 
 out vec4 FragColor;
 
-uniform vec3 lightPos;
 uniform vec3 viewPos;
-uniform vec3 lightColor;
 uniform vec3 objectColor;
 
 void main()
 {
     // Ambient
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
+    vec3 ambientStrength = material.ambient;
+    vec3 ambient = ambientStrength * light.ambient;
 
     // Diffuse
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+    vec3 diffuse = diff * light.diffuse;
 
     // Specular (Blinn-Phong)
-    float specularStrength = 0.5;
+    vec3 specularStrength = material.specular;
     vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = reflect(-lightDir, norm);  
-    float spec = pow(max(dot(norm, reflectDir), 0.0), 256.0);
-    vec3 specular = specularStrength * spec * lightColor;
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(norm, reflectDir), 0.0), material.shininess);
+    vec3 specular = specularStrength * spec * light.specular;
 
-    vec3 result = (ambient + diffuse + specular) * objectColor;
+    vec3 result = (ambient + diffuse + specular);
     FragColor = vec4(result, 1.0);
 }
